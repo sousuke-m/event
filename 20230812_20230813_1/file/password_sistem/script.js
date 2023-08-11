644d1e1c4397df829fc8ebf75865df8318d2c0ce
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2023-08-11 16:09:28 +0900

    Create script.js

---
+++ 20230812_20230813_1/file/password_sistem/script.js
@@ -0,0 +1,15 @@
+function load_(){
+    document.querySelector(".text").select();
+    document.querySelector(".messege").innerHTML = "正しいパスワードを入力してからどれかのキーを押してください";
+}
+window.addEventListener('keydown',keydown__);
+function keydown__(){
+    var pass_ = "14298"
+    if(document.querySelector(".text").value == pass_){
+        console.log(`認識成功:${document.querySelector(".text").value}`);
+        loadAndPlay();
+        document.querySelector(".messege").innerHTML = "認識成功";
+    }else{
+        document.querySelector(".messege").innerHTML = "正しいパスワードを入力してからどれかのキーを押してください";
+    }
+}
